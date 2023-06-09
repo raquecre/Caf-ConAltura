c3bde71fe50aeeedc72a6c84ce68480203be3a38
raquecre <raquecre@gmail.com>
2023-06-09 20:35:37 +0200

    intermediate commits

---
+++ main.js
@@ -0,0 +1,26 @@
+/*-------Preguntas fecuentes-----*/
+//Objetivo: cuando haces click se despliega el menu
+//si vuelves a hacer click se cierra
+
+//Selectores de clase necesarios
+
+
+
+
+//addEventListener → Onclick → Function → bucle → ifElse
+
+
+
+
+
+
+
+
+function myFunctionExample() {
+  const x = document.getElementById("idExample");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
